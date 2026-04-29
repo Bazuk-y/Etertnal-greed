f92ddf8623a193bb0818e16af2cc0d3773b48d80
Bazuk-y <Lukajdalecian@seznam.cz>
2026-04-29 09:29:18 +0200

    Pokus a to decela uspešný o zprovoznění kartiček ypieeee 🃏

--- app/Controllers/Karty.php
+++ app/Controllers/Karty.php
@@ -1,14 +1,22 @@
 <?php
+
 namespace App\Controllers;
+
 use App\Controllers\BaseController;
 use App\Controllers\Karty as KartyModel;
+
 class Karty extends BaseController
 {
-    private $dataTypkomponent; 
+    private $dataTypkomponent;
+
     public function index(){
+        $TKM = new KartyModel();
+        $data = [
+            "karty" => $TKM
+        ];
+        echo view('Karty', $data);
 
     }
-
 }
 
 
--- app/Views/Karty.php
+++ app/Views/Karty.php
@@ -1,1 +1,20 @@
-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae dolorem, repellendus nisi molestias cupiditate iusto beatae optio facere doloribus nulla distinctio pariatur consectetur nam fugit laboriosam vitae alias neque dolore.
+<?= $this->extend('Layout/template') ?>
+<?= $this->section('content'); ?>
+<div class="row">
+<?php
+foreach ($karty as $row) { //kdyz tohle umazu tak facha
+    ?>
+
+<div class="card" style="width:400px">
+    <img class="card-img-top" src="../bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">John Doe</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+
+  <?php
+}
+?>
+  <?= $this->endSection(); ?>
