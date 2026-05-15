6ba5589dd32b3104713ac31c2ab43e834b1d4a62
adamstrizik <toxi999999@gmail.com>
2026-05-15 11:28:41 +0200

    AAAAAA join se stáví a je to utrpení

--- app/Controllers/JednotliveEtapy.php
+++ app/Controllers/JednotliveEtapy.php
@@ -4,12 +4,17 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\Rider;
+use App\Models\RaceYear;
+use App\Models\Result;
+
 
 class JednotliveEtapy extends BaseController
 {
-    public function index2()
+    public function Join()
     {
-       
+       $RaceYear = new RaceYear();
+       $Race = $RaceYear->join("RaceYear","Race_Year.id_race = Result.id_stage","inner")->join("Result","Result.id = Rider.id","inner");
         
         
 
--- app/Models/Rider.php
+++ app/Models/Rider.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class KoloslafRider extends Model
+class Rider extends Model
 {
     protected $table            = 'rider';
     protected $primaryKey       = 'id';
