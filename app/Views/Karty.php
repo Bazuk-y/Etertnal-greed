ad4160dbde508a8e8c053d940db971aa42370f22
Bazuk-y <Lukajdalecian@seznam.cz>
2026-06-05 11:25:47 +0200

    Kraty funguji a hotovy ypieee

--- app/Views/Karty.php
+++ app/Views/Karty.php
@@ -11,12 +11,12 @@ foreach ($fujky  as $row) {
         }
     ?>
     <div class="card" style="width:400px; margin: 10px;">
-        <img class="card-img-top" src="../bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+        <img class="card-img-top" src="<?= base_url("img/" . $row['logo']) ?>" alt="Card image" style="width:100%">
         <div class="card-body">
             <h4 class="card-title"><?= esc($row['year'] ?? 'Neznámý závod') ?></h4>
             <p class="card-text">Celková délka: <?= esc($row['total_distance']) ?> km</p>
-            <p class="card-text">Datum začátku:<?=  $row['start_date'] ?></p>
-            <p class="card-text">Datum Konce:<?=  $row['end_date'] ?></p>
+            <p class="card-text">Datum začátku: <?= date('d.m.Y', strtotime($row['start_date'])) ?></p>
+            <p class="card-text">Datum Konce: <?= date('d.m.Y', strtotime($row['end_date'])) ?></p>
             
             <?= anchor("jednotlive_etapy/" . $row['id_race'], 'Jednotlivé etapy', ['class' => 'btn btn-primary']) ?>      
         </div>
